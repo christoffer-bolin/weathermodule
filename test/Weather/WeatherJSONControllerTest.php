a2fdb71fa6a307a2a11e8c396aefaccd41a26289
Christoffer <christoffer.bohlin@hotmail.com>
2020-11-27 09:45:32 +0000

    Added a cachefolder and changed JSON controller

--- test/Weather/WeatherJSONControllerTest.php
+++ test/Weather/WeatherJSONControllerTest.php
@@ -23,9 +23,9 @@ class WeatherJSONControllerTest extends TestCase
         // di setup
         $this->di = new DIFactoryConfig();
         $this->di->loadServices(ANAX_INSTALL_PATH . "/config/di");
-
+        $this->di->loadServices(ANAX_INSTALL_PATH . "/test/config/di");
         // Use a different cache dir for unit test
-        $this->di->get("cache")->setPath(ANAX_INSTALL_PATH . "/test/cache");
+        //$this->di->get("cache")->setPath(ANAX_INSTALL_PATH . "/test/cache");
 
         // Setup the controller
         $this->controller = new WeatherJSONController();
